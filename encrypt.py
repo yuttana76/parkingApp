9ab4dadffe64d6dd6f74a087e6a091b90f8f8c58
ittiponK <ittipon.krochanapong@gmail.com>
2023-06-20 16:35:08 +0700

    edit encrypt function

--- encrypt.py
+++ encrypt.py
@@ -4,11 +4,12 @@ from Crypto.Cipher import PKCS1_v1_5
 from base64 import b64encode
 
 CERT_PATH = os.environ.get('KTB_CERT_PATH','C:/D/certEncrypt/prod/parkandlive.mrta_98427_prod.cer')
-# Load the public key from file in PEM format
-with open(CERT_PATH, 'rb') as f:
-    public_key = RSA.importKey(f.read())
+
 
 def encrypt(plainText):
+    # Load the public key from file in PEM format
+    with open(CERT_PATH, 'rb') as f:
+        public_key = RSA.importKey(f.read())
     # Use PKCS1_v1_5 to perform RSA encryption with ECB and PKCS1 padding
     cipher = PKCS1_v1_5.new(public_key)
     cipher_text = cipher.encrypt(plainText.encode('utf-8'))
