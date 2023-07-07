5e31a1a8ec26bad3428783bb3654cf8a0f685884
ittiponK <ittipon.krochanapong@gmail.com>
2023-07-07 12:23:08 +0700

    edit print in payment cgp

--- cgpPayment.py
+++ cgpPayment.py
@@ -69,7 +69,7 @@ def paymentCGP(ref1,total,ref2,payRef): #payRef gen from term_seq gen ใหม�
      response = requests.get(url, params=params, cert=cert)
      encoded = (response.text).encode("utf8")
      print(response.status_code)
-     print(response.json())
+     print(response.raw)
      print('response from cgp payment')
      root = ET.fromstring(encoded)
      for child in  root.iter():
